97d276c5d45cb9a4e2621ffe69d518a07763e2d7
xuhb <421271727@qq.com>
2017-10-18 09:41:08 +0800

    添加仓储以及其他功能模块

---
+++ ShuPingNetSolution/ShuPing.Infrastructure/Base/ApiResultModelBase.cs
@@ -0,0 +1,27 @@
+﻿using ShuPing.Infrastructure.StatusEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Base
+{
+    public class ApiResultModelBase
+    {
+        /// <summary>
+        /// 本次调用是否成功
+        /// </summary>
+        public virtual bool IsSuccess { get; set; } 
+
+        /// <summary>
+        /// 返回Message
+        /// </summary>
+        public virtual string Message { get; set; }
+
+        /// <summary>
+        /// 返回状态
+        /// </summary>
+        public virtual StatusCode StatusCode { get; set; }
+    }
+}
---
+++ ShuPingNetSolution/ShuPing.Interface/IRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShuPing.Entity;
+using ShuPing.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuPing.Interface.IRepository
+{
+    public interface IUserRepository : IRepositoryBase<UserEntity>
+    {
+        void DeleteForm(long keyValue);
+        void SubmitForm(UserEntity userEntity, UserLogOnEntity userLogOnEntity, long keyValue);
+    }
+}
