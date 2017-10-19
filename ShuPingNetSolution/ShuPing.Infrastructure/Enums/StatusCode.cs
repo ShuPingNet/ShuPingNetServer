bc0c64343d3a9fe338427c0e96ea0516d70e95cf
xuhb <421271727@qq.com>
2017-10-19 17:48:39 +0800

    增加字符串资源类并优化枚举

--- ShuPingNetSolution/ShuPing.Infrastructure/Enums/StatusCode.cs
+++ ShuPingNetSolution/ShuPing.Infrastructure/Enums/StatusCode.cs
@@ -9,28 +9,52 @@ namespace ShuPing.Infrastructure.StatusEnum
 {
     public enum StatusCodeEnum : int
     {
+        /// <summary>
+        /// 请求(或处理)成功
+        /// </summary>
         [Text("请求(或处理)成功")]
-        Success = 200, //请求(或处理)成功
+        Success = 200,
 
+        /// <summary>
+        /// 内部请求出错
+        /// </summary>
         [Text("内部请求出错")]
-        Error = 500, //内部请求出错
+        Error = 500,
 
+        /// <summary>
+        /// 未授权标识
+        /// </summary>
         [Text("未授权标识")]
-        Unauthorized = 401,//未授权标识
+        Unauthorized = 401,
 
+        /// <summary>
+        /// 请求参数不完整或不正确
+        /// </summary>
         [Text("请求参数不完整或不正确")]
-        ParameterError = 400,//请求参数不完整或不正确
+        ParameterError = 400,
 
+        /// <summary>
+        /// 请求TOKEN失效
+        /// </summary>
         [Text("请求TOKEN失效")]
-        TokenInvalid = 403,//请求TOKEN失效
+        TokenInvalid = 403,
 
+        /// <summary>
+        /// HTTP请求类型不合法
+        /// </summary>
         [Text("HTTP请求类型不合法")]
-        HttpMehtodError = 405,//HTTP请求类型不合法
+        HttpMehtodError = 405,
 
+        /// <summary>
+        /// HTTP请求不合法
+        /// </summary>
         [Text("HTTP请求不合法,请求参数可能被篡改")]
-        HttpRequestError = 406,//HTTP请求不合法
+        HttpRequestError = 406,
 
+        /// <summary>
+        /// HTTP请求不合法
+        /// </summary>
         [Text("该URL已经失效")]
-        URLExpireError = 407,//HTTP请求不合法
+        URLExpireError = 407,
     }
 }
---
+++ ShuPingNetSolution/ShuPing.Infrastructure/Resource/StringResource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Resource
+{
+    public class StringResource
+    {
+        #region 安全验证相关参数
+        /// <summary>
+        /// 唯一标识
+        /// </summary>
+        public static readonly string StaffId = "staffid";
+        /// <summary>
+        /// 时间戳
+        /// </summary>
+        public static readonly string TimeStamp = "timestamp";
+        /// <summary>
+        /// Guid
+        /// </summary>
+        public static readonly string Nonce = "nonce";
+        /// <summary>
+        /// 验签
+        /// </summary>
+        public static readonly string Signature = "signature";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly string GetToken = "GetToken";
+
+        #endregion
+        #region 请求方式
+        public const  string Get = "GET";
+        public const  string Post = "POST";
+        #endregion
+
+
+        #region 日志记录相关参数
+        public const string Stopwatch = "[Stopwatch]";
+        #endregion
+
+    }
+}
