c2e056c23bf684590a04c48a2df5bf6d67f68b67
xuhb <421271727@qq.com>
2017-10-19 17:47:50 +0800

    完善验签

---
+++ ShuPingNetSolution/ShuPing.Infrastructure/Common/WebSettingsConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class WebSettingsConfig
+    {
+        /// <summary>
+        /// 设置过期时间
+        /// </summary>
+        public static string UrlExpireTime
+        {
+            get
+            {
+                return "10";
+            }
+        }
+    }
+}
